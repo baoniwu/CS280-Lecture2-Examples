33128f3a106b8185681ca07d410e3c0c20efcef8
FrankLikuohao <frank.likuohao@gmail.com>
2017-03-07 20:26:58 +0800

    新增浮點數運算

---
+++ Example02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example02
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(0.1f + 0.2f == 0.3f);
+            Console.WriteLine(0.01f + 0.33f == 1.33f);
+            Console.WriteLine(1 / 2);
+            Console.WriteLine((1.0 / 2.0) * 10);
+        }
+    }
+}
